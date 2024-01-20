fe8508b58fa93e854dd1ab21ef88ce3da89d9fb1
onedevmaster <jssmeca@hotmail.com>
2024-01-19 21:50:58 -0300

    Alteração de código

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class ContaTerminal {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         // Solicitar número da agência
